50d825a8c55a5300632ef45b95feebfd0ddc7774
Ercanincee <132362073+Ercanincee@users.noreply.github.com>
2023-11-11 14:57:07 +0300

    Add project files.

---
+++ ZarOyunu/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Zar Atma Oyununa hosgeldiniz Kazanmak için belirtilen sayiden daha yüksek degeri atin");
+do
+{
+   
+    Random dice = new();
+    int sayi = dice.Next(1, 7);
+    Console.WriteLine($"{sayi} sayisindan daha yüksek veya esit zar atiniz");
+    Console.WriteLine("Lutfen konsola zar atmak icin at yaziniz");
+    string onay = Console.ReadLine();
+    if (onay == "at")
+    {
+        int rastgelesayi = dice.Next(1, 7);
+        Console.WriteLine($"{rastgelesayi} attiginiz zarin degeridir");
+        if (rastgelesayi >= sayi)
+        {
+            Console.WriteLine("Kazandınız");
+            Console.WriteLine("Tekrar oynamak icin devam yazınız");
+            string devammi = Console.ReadLine();
+            if (devammi == "devam") {
+                continue;
+            } else { break; }
+        }
+        else { Console.WriteLine("Kaybettiniz"); }
+    }
+    else
+    {
+        Console.WriteLine("Lütfen zar atmak icin konsola at yaziniz");
+    }
+
+} while (true);
+
